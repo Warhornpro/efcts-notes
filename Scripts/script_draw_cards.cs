eae35087b57fde86c5ad1bd46bdc0f85215ec0d6
Warhornpro <56305914+Warhornpro@users.noreply.github.com>
2020-11-06 09:54:48 -0500

    Add files via upload

---
+++ Scripts/script_draw_cards.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_draw_cards : MonoBehaviour
+{
+    public GameObject Card_Insert;
+    public GameObject Card_Insert2;
+    public GameObject Player_Area;
+    public GameObject Enemy_Area;
+    
+        void Start()
+    {
+        
+    }
+    public void onClick()
+    {
+        for (var i = 0; i < 5; i++)
+        { 
+        GameObject playerCard = Instantiate(Card_Insert, new Vector3(0, 0, 0), Quaternion.identity);
+        playerCard.transform.SetParent(Player_Area.transform, false);
+    }
+    }
+
+}
